98c24966bcef72cd85b5bb1fb6db34632572aed8
Pier-Yves Lessard <py.lessard@gmail.com>
2017-11-25 20:52:21 -0800

    Added configuration files

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'udsoncan',
+  packages = ['udsoncan'],
+  version = '0.1',
+  description = 'Implementation of the Unified Diagnostic Service (UDS) protocol (ISO-14229) used in the automotive industry.',
+  long_description=long_description,
+  author = 'Pier-Yves Lessard',
+  author_email = 'py.lessard@gmail.com',
+  license='MIT',
+  url = 'https://github.com/pylessard/python-udsoncan',
+  download_url = 'https://github.com/pylessard/python-udsoncan/archive/v0.1.tar.gz',
+  keywords = ['uds', '14229', 'iso-14229', 'diagnostic', 'automotive'], 
+  python_requires='>=3.7',
+  classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        ],
+)
