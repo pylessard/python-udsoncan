eddc90015717992da764b3b52e76a16ffc5c1e87
Pier-Yves Lessard <py.lessard@gmail.com>
2017-11-25 20:54:27 -0800

    Removed development test files

--- testclient.py
+++
@@ -1,8 +0,0 @@
-from udsoncan import services, Request, Connection
-from udsoncan.client import Client
-from testconfig import client_config
-
-conn = Connection('vcan0', rxid=0x123, txid=0x456)
-
-with Client(conn, request_timeout=1, config=client_config) as client:
-	client.ecu_reset(services.ECUReset.enableRapidPowerShutDown, 1)
