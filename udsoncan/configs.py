1a33443ffa6eed6ed18abe6f1f885e7a190903b7
Pier-Yves Lessard <py.lessard@gmail.com>
2018-02-26 21:05:17 -0800

    Removed debug info in config file

--- udsoncan/configs.py
+++ udsoncan/configs.py
@@ -1,15 +1,3 @@
 default_client_config  = {
-#
-#	'IO' : {
-#		0x0123  : {
-#			'codec' : codec,
-#			'mask' : {
-#				'aaa' : 0x10,
-#				'bbb' : 0x20
-#			}
-#		}
-#	}
+
 }
-
-
-#client.iocontrol(0x123, ShortTermAdjustment, value=IoValues(aaa=888, bbb=999), IoMask(aaa=False))
