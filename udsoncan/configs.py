17a1bba0b359ee4aa8c6bffe87ec2e6b3a4f3f7a
Pier-Yves Lessard <py.lessard@gmail.com>
2018-04-08 21:01:22 -0700

    Added some default configuration to default_client_config

--- udsoncan/configs.py
+++ udsoncan/configs.py
@@ -1,5 +1,9 @@
 default_client_config  = {
-	'tolerate_zero_padding' : True,
-	'ignore_all_zero_dtc' : True,
-	'dtc_snapshot_did_size' : 2		# Not specified in standard. 2 bytes matches other services format.
+	'security_algo'				: None,
+	'security_algo_params'		: None,
+	'tolerate_zero_padding' 	: True,
+	'ignore_all_zero_dtc' 		: True,
+	'dtc_snapshot_did_size' 	: 2,		# Not specified in standard. 2 bytes matches other services format.
+	'server_address_format'		: None,		# 8,16,24,32,40
+	'server_memorysize_format'	: None		# 8,16,24,32,40
 }
