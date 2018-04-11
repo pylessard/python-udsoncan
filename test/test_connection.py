78f3ea756a793b057a51c2bdfab140e8f2389473
Pier-Yves Lessard <py.lessard@gmail.com>
2018-04-10 20:30:05 -0700

    Added SocketConnection

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,6 +1,9 @@
 from test.UdsTest import UdsTest
-from udsoncan.Connection import IsoTPConnection
+from udsoncan.Connection import IsoTPConnection, SocketConnection
 from test.stub import StubbedIsoTPSocket
+import socket
+import threading
+import time
 
 class TestIsoTPConnection(UdsTest):
 
@@ -25,4 +28,66 @@ class TestIsoTPConnection(UdsTest):
 				payload1 = b"\x00\x01\x02\x03\x04"
 				conn1.send(payload1)
 				payload2 = conn2.wait_frame(timeout=0.3)
+				self.assertEqual(payload1, payload2)
+
+class TestSocketConnection(UdsTest):
+	def server_sock_thread_task(self):
+		self.thread_started=True
+		self.sock1, addr = self.server_sock.accept()
+
+	def setUp(self):
+		self.thread_started = False
+		self.server_sock_thread = threading.Thread(target=self.server_sock_thread_task)
+
+		self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server_sock.setblocking(False)
+		self.sock1 = None
+		self.sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+		self.server_sock.settimeout(0.5)
+
+		self.server_sock.bind(('127.0.0.1', 0))
+		self.server_sock.listen(1)
+		self.server_sock_thread.start()
+
+		t1 = time.time()
+		while not self.thread_started:
+			if (time.time() - t1) > 0.5:
+				raise RuntimeError('Timeout while connecting sockets together.')
+			time.sleep(0.01)
+		time.sleep(0.01)
+
+		self.sock2.connect(self.server_sock.getsockname())
+		t1 = time.time()
+		while self.sock1 is None:
+			if (time.time() - t1) > 0.5:
+				raise RuntimeError('Timeout while connecting sockets together.')
+
+	def tearDown(self):
+		if isinstance(self.sock1, socket.socket):
+			self.sock1.close()
+		
+		if isinstance(self.sock2, socket.socket):
+			self.sock2.close()
+
+		if isinstance(self.server_sock, socket.socket):
+			self.server_sock.close()
+		
+	def test_open(self):
+		conn = SocketConnection(self.sock1)
+		self.assertFalse(conn.is_open())
+		conn.open()
+		self.assertTrue(conn.is_open())
+		conn.close()
+		self.assertFalse(conn.is_open())
+
+	def test_transmit(self):
+		conn1 = SocketConnection(self.sock1)
+		conn2 = SocketConnection(self.sock2)
+		
+		with conn1.open():
+			with conn2.open():
+				payload1 = b"\x00\x01\x02\x03\x04"
+				conn1.send(payload1)
+				payload2 = conn2.wait_frame(timeout=1, exception=True)
 				self.assertEqual(payload1, payload2)
--- udsoncan/Connection.py
+++ udsoncan/Connection.py
@@ -6,6 +6,80 @@ from udsoncan.Request import Request
 from udsoncan.Response import Response
 from udsoncan import TimeoutException
 
+class SocketConnection(object):
+	def __init__(self, sock, bufsize=4095):
+		self.rxqueue = queue.Queue()
+		self.exit_requested = False
+		self.opened = False
+		self.rxthread = threading.Thread(target=self.rxthread_task)
+		self.sock = sock
+		self.sock.settimeout(0.1)	# for recv
+		self.bufsize=bufsize
+
+
+	def open(self):
+		self.exit_requested = False
+		self.rxthread.start()
+		self.opened = True
+		return self
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, type, value, traceback):
+		self.close()
+
+	def is_open(self):
+		return self.opened
+
+	def rxthread_task(self):
+		while not self.exit_requested:
+			try:
+				data = self.sock.recv(self.bufsize)
+				if data is not None:
+					self.rxqueue.put(data)
+			except socket.timeout as e:
+				pass
+			except Exception as e:
+				self.exit_requested = True
+
+
+	def close(self):
+		self.exit_requested = True
+		self.opened = False
+
+	def send(self, obj):
+		if isinstance(obj, Request) or isinstance(obj, Response):
+			payload = obj.get_payload()  
+		else :
+			payload = obj
+
+		self.sock.send(payload)
+
+	def wait_frame(self, timeout=2, exception=False):
+		if not self.opened:
+			if exception:
+				raise RuntimeError("Connection is not opened")
+			else:
+				return None
+
+		timedout = False
+		frame = None
+		try:
+			frame = self.rxqueue.get(block=True, timeout=timeout)
+
+		except queue.Empty:
+			timedout = True
+			
+		if exception and timedout:
+			raise TimeoutException("Did not received frame in time (timeout=%s sec)" % timeout)
+
+		return frame
+
+	def empty_rxqueue(self):
+		while not self.rxqueue.empty():
+			self.rxqueue.get()
+
 class IsoTPConnection(object):
 	def __init__(self, interface, rxid, txid, tpsock=None):
 		import isotp
