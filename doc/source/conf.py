97667b8cf672f8c7b66e7f7eab9fabf94563e543
Pier-Yves Lessard <py.lessard@gmail.com>
2023-01-08 21:48:18 -0500

    Release 1.16

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -59,7 +59,7 @@ author = u'Pier-Yves Lessard'
 # built documents.
 #
 # The short X.Y version.
-version = u'1.15'
+version = u'1.16'
 # The full version, including alpha/beta/rc tags.
 release = u'0'
 
