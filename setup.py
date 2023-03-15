1b0784813c0e8c01f8f4bef909039237ec881676
Pier-Yves Lessard <py.lessard@gmail.com>
2023-03-14 20:38:04 -0400

    V1.17

--- setup.py
+++ setup.py
@@ -8,22 +8,22 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name = 'udsoncan',
-  packages = find_packages(exclude=['test']),
-  package_data={
-      '': ['*.conf'],
-  },
-  version = '1.16',
-  description = 'Implementation of the Unified Diagnostic Service (UDS) protocol (ISO-14229) used in the automotive industry.',
-  long_description=long_description,
-  author = 'Pier-Yves Lessard',
-  author_email = 'py.lessard@gmail.com',
-  license='MIT',
-  url = 'https://github.com/pylessard/python-udsoncan',
-  download_url = 'https://github.com/pylessard/python-udsoncan/archive/v1.16.tar.gz',
-  keywords = ['uds', '14229', 'iso-14229', 'diagnostic', 'automotive'], 
-  python_requires='>=3.0',
-  classifiers = [
+    name='udsoncan',
+    packages=find_packages(exclude=['test']),
+    package_data={
+        '': ['*.conf'],
+    },
+    version='1.17',
+    description='Implementation of the Unified Diagnostic Service (UDS) protocol (ISO-14229) used in the automotive industry.',
+    long_description=long_description,
+    author='Pier-Yves Lessard',
+    author_email='py.lessard@gmail.com',
+    license='MIT',
+    url='https://github.com/pylessard/python-udsoncan',
+    download_url='https://github.com/pylessard/python-udsoncan/archive/v1.17.tar.gz',
+    keywords=['uds', '14229', 'iso-14229', 'diagnostic', 'automotive'],
+    python_requires='>=3.0',
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
@@ -31,5 +31,5 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
-        ],
+    ],
 )
