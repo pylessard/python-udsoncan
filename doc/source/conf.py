1232118bb259d39fc147e785fa2fb14c05a0f169
Pier-Yves Lessard <py.lessard@gmail.com>
2024-03-04 21:48:18 -0500

    Release V1.23.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -60,7 +60,7 @@ author = u'Pier-Yves Lessard'
 # built documents.
 #
 # The short X.Y version.
-version = u'1.22'
+version = u'1.23'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ setup(
         'test': ['mypy', 'coverage'],
         'dev': ['mypy', 'ipdb', 'autopep8', 'coverage']
     },
-    version='1.22.1',
+    version='1.23.0',
     description='Implementation of the Unified Diagnostic Service (UDS) protocol (ISO-14229) used in the automotive industry.',
     long_description=long_description,
     author='Pier-Yves Lessard',
     author_email='py.lessard@gmail.com',
     license='MIT',
     url='https://github.com/pylessard/python-udsoncan',
-    download_url='https://github.com/pylessard/python-udsoncan/archive/v1.22.1.tar.gz',
+    download_url='https://github.com/pylessard/python-udsoncan/archive/v1.23.0.tar.gz',
     keywords=['uds', '14229', 'iso-14229', 'diagnostic', 'automotive'],
     python_requires='>=3.7',
     classifiers=[
