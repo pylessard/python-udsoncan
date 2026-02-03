192e7e0ef00f7833b9fcf1f1344c30b6972551c5
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-02-03 18:08:58 -0500

    build: Migrate to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-from codecs import open
 from os import path
 import sys
 
@@ -8,37 +7,6 @@ here = path.abspath(path.dirname(__file__))
 sys.path.insert(0, here)
 import udsoncan
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
-    name='udsoncan',
-    packages=find_packages(exclude=["test", "test.*"], include=['udsoncan', "udsoncan.*"]),
-    package_data={
-        '': ['*.conf'],
-        'udsoncan' : ['py.typed']
-    },
-    extras_require={
-        'test': ['mypy', 'coverage'],
-        'dev': ['mypy', 'ipdb', 'autopep8', 'coverage']
-    },
     version=udsoncan.__version__,
-    description='Implementation of the Unified Diagnostic Service (UDS) protocol (ISO-14229) used in the automotive industry.',
-    long_description=long_description,
-    author='Pier-Yves Lessard',
-    author_email='py.lessard@gmail.com',
-    license='MIT',
-    url='https://github.com/pylessard/python-udsoncan',
-    download_url=f'https://github.com/pylessard/python-udsoncan/archive/v{udsoncan.__version__}.tar.gz',
-    keywords=['uds', '14229', 'iso-14229', 'diagnostic', 'automotive'],
-    python_requires='>=3.7',
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "Operating System :: POSIX :: Linux",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
-    ],
 )
